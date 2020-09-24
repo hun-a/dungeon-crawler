6fedd0dd626a2e4817c7a591b3af1d6afc08a0cc
hun-a <seunghun.chan@gmail.com>
2020-09-25 01:59:19 +0900

    Adding the animations for Faune

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -31,5 +31,38 @@ export default class Game extends Phaser.Scene {
       key: 'faune-idle-down',
       frames: [{ key: 'faune', frame: 'walk-down-3.png' }]
     });
+
+    this.anims.create({
+      key: 'faune-idle-up',
+      frames: [{ key: 'faune', frame: 'walk-up-3.png' }]
+    });
+
+    this.anims.create({
+      key: 'faune-idle-side',
+      frames: [{ key: 'faune', frame: 'walk-side-3.png' }]
+    });
+
+    this.anims.create({
+      key: 'faune-run-down',
+      frames: this.anims.generateFrameNames('faune', { start: 1, end: 8, prefix: 'run-down-', suffix: '.png' }),
+      repeat: -1,
+      frameRate: 15
+    });
+
+    this.anims.create({
+      key: 'faune-run-up',
+      frames: this.anims.generateFrameNames('faune', { start: 1, end: 8, prefix: 'run-up-', suffix: '.png' }),
+      repeat: -1,
+      frameRate: 15
+    });
+
+    this.anims.create({
+      key: 'faune-run-side',
+      frames: this.anims.generateFrameNames('faune', { start: 1, end: 8, prefix: 'run-side-', suffix: '.png' }),
+      repeat: -1,
+      frameRate: 15
+    });
+
+    faune.anims.play('faune-idle-down');
   }
 }
