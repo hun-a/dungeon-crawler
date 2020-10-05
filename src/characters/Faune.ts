258d27369e7d2ad17a9bee3a59348c9bae8ea387
hun-a <seunghun.chan@gmail.com>
2020-10-05 22:16:18 +0900

    Fixed faune's anims bug

--- src/characters/Faune.ts
+++ src/characters/Faune.ts
@@ -48,6 +48,7 @@ export default class Faune extends Phaser.Physics.Arcade.Sprite {
   }
 
   preUpdate(t: number, dt: number) {
+    super.preUpdate(t, dt);
     switch (this.healthState) {
       case HealthState.IDLE:
         break;
