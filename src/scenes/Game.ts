d6962e3fb7850db1b9566b6db0524a437da216bd
hun-a <seunghun.chan@gmail.com>
2020-10-08 01:31:05 +0900

    Added the simple chest

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 import { debugDraw } from "../utils/debug";
 import { createLizardAnims } from "../anims/EnemyAnims";
 import { createCharacterAnims } from "../anims/CharacterAnims";
+import { createChestAnims } from "../anims/ChestAnims";
 import Lizard from "../enemies/Lizard";
 import Faune from "../characters/Faune";
 import "../characters/Faune";
@@ -33,11 +34,22 @@ export default class Game extends Phaser.Scene {
 
     createCharacterAnims(this.anims);
     createLizardAnims(this.anims);
+    createChestAnims(this.anims);
 
     const map = this.make.tilemap({ key: "dungeon" });
     const tileset = map.addTilesetImage("dungeon", "tiles", 16, 16, 1, 2);
 
     map.createStaticLayer("Ground", tileset);
+
+    const chest = this.add.sprite(
+      64,
+      64,
+      "treasure",
+      "chest_empty_open_anim_f0.png"
+    );
+    this.time.delayedCall(1000, () => {
+      chest.play("chest-open");
+    });
 
     this.knives = this.physics.add.group({
       classType: Phaser.Physics.Arcade.Image,
